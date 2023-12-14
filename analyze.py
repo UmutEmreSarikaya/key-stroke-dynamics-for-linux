87c662f77a2e97f499ef408a2234f97e2f0d71c5
Umut Emre Sarikaya <61623237+UmutEmreSarikaya@users.noreply.github.com>
2023-12-14 14:33:47 +0300

    Combined analyze.py & calculate_averages.py

--- analyze.py
+++ analyze.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 df = pd.read_csv("keylogger_results.csv")
 
@@ -8,17 +9,38 @@ down_events = df[df["Event"] == "down"].copy()
 up_events = df[df["Event"] == "up"].copy()
 #print(up_events)
 
-result_df = pd.DataFrame()
-result_df["User"] = down_events["User"].values
-result_df["Key"] = down_events["Key"].values
-result_df["TimeDown"] = down_events["TimeInMillis"].values
-result_df["TimeUp"] = up_events["TimeInMillis"].values
-result_df["H"] = up_events["TimeInMillis"].values - down_events["TimeInMillis"].values
+analyzed_df = pd.DataFrame()
+analyzed_df["User"] = down_events["User"].values
+analyzed_df["Key"] = down_events["Key"].values
+analyzed_df["TimeDown"] = down_events["TimeInMillis"].values
+analyzed_df["TimeUp"] = up_events["TimeInMillis"].values
+analyzed_df["H"] = up_events["TimeInMillis"].values - down_events["TimeInMillis"].values
+analyzed_df["DD"] = down_events["TimeInMillis"].diff().values
+analyzed_df["UU"] = up_events["TimeInMillis"].diff().values
+analyzed_df["UD"] = analyzed_df["UU"].values - analyzed_df["H"].values
 
-result_df["DD"] = down_events["TimeInMillis"].diff().values
-result_df["UU"] = up_events["TimeInMillis"].diff().values
-result_df["UD"] = result_df["UU"].values - result_df["H"].values
+analyzed_df.to_csv("analyzed_results.csv", index=False)
 
-pd.set_option('display.max_columns', None)
-#print(result_df)
-result_df.to_csv("analyzed_results.csv", index=False)
+processed_df = pd.DataFrame(columns=["H", "DD", "UU", "UD", "Key_stroke_average", "Label"])
+
+start_time = analyzed_df["TimeDown"].iloc[0]
+end_time = analyzed_df["TimeUp"].iloc[-1]
+username = analyzed_df.iloc[0]["User"]
+
+total_time = end_time - start_time
+
+processed_df.loc[0, "H"] = analyzed_df["H"].mean()
+processed_df.loc[0, "DD"] = analyzed_df["DD"].mean()
+processed_df.loc[0, "UU"] = analyzed_df["UU"].mean()
+processed_df.loc[0, "UD"] = analyzed_df["UD"].mean()
+processed_df.loc[0, "Key_stroke_average"] = (len(analyzed_df)/total_time)*500 #number of keys pressed in 500 ms(half a second)
+processed_df.loc[0, "Label"] = username
+
+file_path = "key_stroke_data.csv"
+
+if os.path.exists(file_path):
+    include_header = False
+else:
+    include_header = True
+
+processed_df.to_csv(file_path, header=include_header, index=False, mode='a')
