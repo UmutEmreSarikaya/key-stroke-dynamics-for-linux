6e7f1f23b9e99a0b8a2e93213505c4c2ee5e5fea
Umut Emre Sarikaya <61623237+UmutEmreSarikaya@users.noreply.github.com>
2023-12-18 21:34:19 +0300

    modified analyze.py

--- analyze.py
+++ analyze.py
@@ -22,7 +22,7 @@ analyzed_df["UD"] = analyzed_df["UU"].values - analyzed_df["H"].values
 
 analyzed_df.to_csv("analyzed_results.csv", index=False)
 
-processed_df = pd.DataFrame(columns=["H", "DD", "UU", "UD", "Key_stroke_average", "Label"])
+processed_df = pd.DataFrame(columns=["H", "DD", "UD", "key_stroke_average", "back_space_count", "used_caps", "label"])
 
 start_time = analyzed_df["TimeDown"].iloc[0]
 end_time = analyzed_df["TimeUp"].iloc[-1]
@@ -32,10 +32,15 @@ total_time = end_time - start_time
 
 processed_df.loc[0, "H"] = analyzed_df["H"].mean()
 processed_df.loc[0, "DD"] = analyzed_df["DD"].mean()
-processed_df.loc[0, "UU"] = analyzed_df["UU"].mean()
 processed_df.loc[0, "UD"] = analyzed_df["UD"].mean()
-processed_df.loc[0, "Key_stroke_average"] = (len(analyzed_df)/total_time)*500 #number of keys pressed in 500 ms(half a second)
-processed_df.loc[0, "Label"] = username
+processed_df.loc[0, "key_stroke_average"] = (len(analyzed_df)/total_time)*500 #number of keys pressed in 500 ms(half a second)
+processed_df.loc[0, "back_space_count"] = (analyzed_df["Key"].value_counts().get("BACK_SPACE", 0)/total_time)*500 #number of BACK_SPACE pressed in 500 ms(half a second)
+if "CAPS_LOCK" in analyzed_df["Key"].values:
+    used_caps = 1
+else:
+    used_caps = 0
+processed_df.loc[0, "used_caps"] = used_caps
+processed_df.loc[0, "label"] = username
 
 file_path = "key_stroke_data.csv"
 
